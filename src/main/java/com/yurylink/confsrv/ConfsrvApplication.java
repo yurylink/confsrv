deb43aa633e29b47e113a4324ea746267a22aef7
Wesley Yuri <Wesley.yury@4ward.com.br>
2019-06-20 14:05:32 -0300

    Initial commit

---
+++ src/main/java/com/yurylink/confsrv/ConfsrvApplication.java
@@ -0,0 +1,13 @@
+package com.yurylink.confsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfsrvApplication.class, args);
+	}
+
+}
