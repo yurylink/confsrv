c4e90e24fe8587aa9eb8e08ac0d40cfc24dfc8b1
Wesley Almeida <yurylink@hotmail.com>
2019-07-03 21:00:54 -0300

    Configuration server using git repository

--- src/main/java/com/yurylink/confsrv/ConfsrvApplication.java
+++ src/main/java/com/yurylink/confsrv/ConfsrvApplication.java
@@ -3,6 +3,8 @@ package com.yurylink.confsrv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @SpringBootApplication
 @EnableConfigServer
